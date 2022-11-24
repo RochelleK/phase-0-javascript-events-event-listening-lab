b823e63601fa695a12fa6a73440a0ee5663b09be
Rochelle Katzman <zelrochelle@gmail.com>
2022-11-24 15:41:32 -0500

    commit tree 124b80fe3e6dd6d15ceb4069ad383840b73e1168

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
 function addingEventListener() {
+    const btn = document.getElementById('button')
+
+    btn.addEventListener("click", function(){
+        alert('I was clicked!')
+
+
+    })
+    
+    
 }
+addingEventListener()
